7edf18a987b378ae1e93b4843287541ac44c4ee5
ivol84 <ivol84@gmail.com>
2016-05-24 17:28:29 +0300

    Add configuration.

--- src/Config/ConfigurationFactory.php
+++ src/Config/ConfigurationFactory.php
@@ -1,12 +1,21 @@
 <?php
 
-namespace Config;
+namespace ivol\Config;
 
+
+use Symfony\Component\Config\Definition\Processor;
 
 class ConfigurationFactory
 {
-    public static function createDefault()
+
+    public static function createFromArray($params = array())
     {
-        
+        $processor = new Processor();
+        $configuration = new ExecWrapperConfiguration();
+        $processedConfiguration = $processor->processConfiguration(
+            $configuration,
+            array($params)
+        );
+        return $processedConfiguration;
     }
 }
